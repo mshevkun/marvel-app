eebc654f62c5d020811474e1f7135db4d92153b6
Maks <mshevkun.dev@gmail.com>
2023-09-13 11:23:14 -0400

    Implemented advanced routing, optimized performance with memoization, and introduced state management enhancements

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,46 +4,42 @@ import { Helmet } from "react-helmet";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-import CharSearchForm from '../charSearchForm/CharSearchForm';
+import CharSearchForm from "../charSearchForm/CharSearchForm";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
-import decoration from '../../resources/img/vision.png';
+import decoration from "../../resources/img/vision.png";
 
 const MainPage = () => {
+  const [selectedChar, setChar] = useState(null);
 
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-    return (
-        <>
-            <Helmet>
-                <meta
-                    name="description"
-                    content="Marvel information portal"
-                    />
-                <title>Marvel information portal</title>
-            </Helmet>
-            <ErrorBoundary>
-                <RandomChar/>
-            </ErrorBoundary>
-            <div className="char__content">
-                <ErrorBoundary>
-                    <CharList onCharSelected={onCharSelected}/>
-                </ErrorBoundary>
-                <div>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharSearchForm/>
-                    </ErrorBoundary>
-                </div>
-            </div>
-            <img className="bg-decoration" src={decoration} alt="vision"/>
-        </>
-    )
-}
+  const onCharSelected = (id) => {
+    setChar(id);
+  };
+  return (
+    <>
+      <Helmet>
+        <meta name="description" content="Marvel app" />
+        <title>Marvel app</title>
+      </Helmet>
+      <ErrorBoundary>
+        <RandomChar />
+      </ErrorBoundary>
+      <div className="char__content">
+        <ErrorBoundary>
+          <CharList onCharSelected={onCharSelected} />
+        </ErrorBoundary>
+        <div>
+          <ErrorBoundary>
+            <CharInfo charId={selectedChar} />
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <CharSearchForm />
+          </ErrorBoundary>
+        </div>
+      </div>
+      <img className="bg-decoration" src={decoration} alt="vision" />
+    </>
+  );
+};
 
 export default MainPage;
